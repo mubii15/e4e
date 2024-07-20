4d29a53b2c5b3b5592d92ffa2da0234b2c25e663
Mubarak Ismail <mubi15@hotmail.com>
2024-07-20 18:58:13 +0100

    Fixed Issue With Ages on Bar Chart

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,13 @@
 <template>
-    <header class="d-flex justify-content-center align-items-center" style="height: 10vh;margin-bottom: 20px;background: #082c32;">
-        <div class="d-flex justify-content-center align-items-center" style="height: 100%;object-fit: contain;width: 200px;padding: 10px;overflow: hidden;"><img style="width: 100%;" src="assets/img/ehealth_teal_green_70px.png"></div>
-        <div v-if="!online" class="offline-indicator" style="position: absolute; top: 0; right: 0; padding: 10px; color: white; background: red;">
+  <header class="d-flex justify-content-center align-items-center" style="height: 10vh;margin-bottom: 20px;background: #082c32;">
+    <div class="d-flex justify-content-center align-items-center" style="height: 100%;object-fit: contain;width: 200px;padding: 10px;overflow: hidden;">
+      <img style="width: 100%;" src="assets/img/ehealth_teal_green_70px.png">
+    </div>
+    <div v-if="!online" class="offline-indicator" style="position: absolute; top: 0; right: 0; padding: 10px; color: white; background: red;">
       You are offline
     </div>
-      </header>  
-    <main class="d-flex justify-content-center align-items-center" style="min-height: 90vh;width: 100%;margin: 0px;">
+  </header>  
+  <main class="d-flex justify-content-center align-items-center" style="min-height: 90vh;width: 100%;margin: 0px;">
     <div class="container">
       <div class="row" style="margin-bottom: 25px;background: rgba(24,132,150,0.11);">
         <div class="col" style="box-shadow: 2px 3px 3px 1px rgba(33,37,41,0.11);border-radius: 10px;border: 1px solid #188496;">
@@ -14,17 +16,17 @@
       </div>
       <div class="row" style="margin-bottom:50px;">
         <div class="col" style="border-radius: 10px;box-shadow: 2px 3px 3px 1px rgba(33,37,41,0.11);border: 1px solid #188496;">
-                    <div class="row" style="margin-bottom: 15px;padding-top: 5px;padding-bottom: 5px;background: #188496;border-top-left-radius: 10px;border-top-right-radius: 10px;">
-                        <div class="col d-flex justify-content-center align-items-center">
-                            <h6 style="color: rgb(255,255,255);">Name</h6>
-                        </div>
-                        <div class="col d-flex justify-content-center align-items-center">
-                            <h6 style="color: rgb(255,255,255);">Age</h6>
-                        </div>
-                        <div class="col d-flex justify-content-center align-items-center">
-                            <h6 style="color: rgb(255,255,255);">Blood Group</h6>
-                        </div>
-                    </div>
+          <div class="row" style="margin-bottom: 15px;padding-top: 5px;padding-bottom: 5px;background: #188496;border-top-left-radius: 10px;border-top-right-radius: 10px;">
+            <div class="col d-flex justify-content-center align-items-center">
+              <h6 style="color: rgb(255,255,255);">Name</h6>
+            </div>
+            <div class="col d-flex justify-content-center align-items-center">
+              <h6 style="color: rgb(255,255,255);">Age</h6>
+            </div>
+            <div class="col d-flex justify-content-center align-items-center">
+              <h6 style="color: rgb(255,255,255);">Blood Group</h6>
+            </div>
+          </div>
           <div v-for="person in people" :key="person.id" class="row" style="background: #ffffff;padding-top: 10px;padding-bottom: 10px;border-bottom: 1px solid rgb(186,186,186);">
             <div class="col d-flex justify-content-center align-items-center">
               <h6 style="margin: 0px;">{{ person.name.first + " " + person.name.last }}</h6>
@@ -114,74 +116,33 @@ export default {
             borderWidth: 0
           }
         },
+        legend: {
+        title: {
+          text: 'Age'
+        }
+      },
         series: [{
           name: '0-18',
-          data: [ageGroups['0-18'], 0, 0, 0, 0, 0, 0, 0],
-          color: '#7cb5ec',
-          showInLegend: false
+          data: Object.keys(bloodGroups).map(bloodGroup => data.filter(person => person.age <= 18 && person.bloodGroup === bloodGroup).length),
+          color: '#7cb5ec'
         }, {
           name: '19-35',
-          data: [0, ageGroups['19-35'], 0, 0, 0, 0, 0, 0],
-          color: '#434348',
-          showInLegend: false
+          data: Object.keys(bloodGroups).map(bloodGroup => data.filter(person => person.age > 18 && person.age <= 35 && person.bloodGroup === bloodGroup).length),
+          color: '#434348'
         }, {
           name: '36-50',
-          data: [0, 0, ageGroups['36-50'], 0, 0, 0, 0, 0],
-          color: '#90ed7d',
-          showInLegend: false
+          data: Object.keys(bloodGroups).map(bloodGroup => data.filter(person => person.age > 35 && person.age <= 50 && person.bloodGroup === bloodGroup).length),
+          color: '#90ed7d'
         }, {
           name: '51+',
-          data: [0, 0, 0, ageGroups['51+'], 0, 0, 0, 0],
-          color: '#f7a35c',
-          showInLegend: false
-        }, {
-          name: 'A+',
-          data: [bloodGroups['A+']],
-          color: '#7cb5ec',
-          showInLegend: true
-        }, {
-          name: 'B+',
-          data: [bloodGroups['B+']],
-          color: '#434348',
-          showInLegend: true
-        }, {
-          name: 'AB+',
-          data: [bloodGroups['AB+']],
-          color: '#90ed7d',
-          showInLegend: true
-        }, {
-          name: 'O+',
-          data: [bloodGroups['O+']],
-          color: '#f7a35c',
-          showInLegend: true
-        }, {
-          name: 'A-',
-          data: [bloodGroups['A-']],
-          color: '#f45b5b',
-          showInLegend: true
-        }, {
-          name: 'B-',
-          data: [bloodGroups['B-']],
-          color: '#91e8e1',
-          showInLegend: true
-        }, {
-          name: 'AB-',
-          data: [bloodGroups['AB-']],
-          color: '#e4d354',
-          showInLegend: true
-        }, {
-          name: 'O-',
-          data: [bloodGroups['O-']],
-          color: '#2b908f',
-          showInLegend: true
+          data: Object.keys(bloodGroups).map(bloodGroup => data.filter(person => person.age > 50 && person.bloodGroup === bloodGroup).length),
+          color: '#f7a35c'
         }]
       });
     }
   }
 };
 </script>
-
-
 
 <style scoped>
 .offline-indicator {
@@ -201,9 +162,10 @@ ul {
 }
 li {
   display: inline-block;
-  margin: 0 10px;
+  margin: 10px;
 }
 a {
-  color: #42b983;
+  text-decoration: none;
+  color: #3b8070;
 }
 </style>
--- src/indexedDBService.js
+++ src/indexedDBService.js
@@ -2,20 +2,28 @@ import { openDB } from 'idb';
 
 const dbPromise = openDB('vue-app-db', 1, {
   upgrade(db) {
-    db.createObjectStore('people', { keyPath: 'id' }); // Use the correct keyPath
+    db.createObjectStore('people', { keyPath: 'id' });
   },
 });
 
 export const addPeopleToDB = async (people) => {
   const db = await dbPromise;
   const tx = db.transaction('people', 'readwrite');
+  const store = tx.store;
+
   people.forEach(person => {
-    tx.store.put(person); // Use put instead of add to update existing entries
+    // Ensure person has an 'id' field; otherwise, you can't store it correctly
+    if (person.id) {
+      store.put(person); // Use put() to update or add data with the keyPath 'id'
+    } else {
+      console.error('Person object missing ID:', person);
+    }
   });
+
   await tx.done;
 };
 
 export const getPeopleFromDB = async () => {
   const db = await dbPromise;
-  return db.getAll('people'); // Ensure store name matches
+  return db.getAll('people');
 };
